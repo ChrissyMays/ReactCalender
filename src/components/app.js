552ab0311506774deb5c09d14a707f4dc3ef44ec
crystalmays <chrissymays95@gmail.com>
2022-01-20 22:18:41 -0500

    build skeleton for content wrapper

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+// components
 import Header from "./header"
+import ContentWrapper from './content/contentWrapper';
 import Footer from "./footer"
-
+// raw js data not called as component
 import dummyData from '../../static/assets/dummyData';
+
 
 export default class App extends Component {
 constructor(){
@@ -18,7 +21,13 @@ constructor(){
     return (
       <div className='page-wrapper'>
         <Header month= {this.state.monthData.name} />
+        <ContentWrapper 
+        daysInMonth= {this.state.monthData.days_in_month}
+        daysInPreviousMonth= {this.state.monthData.days_in_previous_month}
+        startDay= {this.state.monthData.start_day}
+        />
         <Footer footerdata= {this.state.monthData.year}/>
+
       </div>
     );
   }
